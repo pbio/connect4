cec28caa2efd78d9e1e96eb25f9286533d2b75dc
Paul Bioche <pebioche@gmail.com>
2023-02-06 11:56:14 +0100

    diag check winner works

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -12,23 +12,22 @@ export default function Game(){
 
     function clickedColumn(id:number){
         //check if column is full
-        console.log(currentBoardLayout[id])
         if (currentBoardLayout[id].length == NUMBER_OF_ROWS) {
-            alert('column is full, pick another');
+            alert('column is full, pick another'); //use a state for this;
             return;
         }
         const color = isRedTurn ? 'r' : 'y';
         const newBoardLayout = [...currentBoardLayout];
         newBoardLayout[id] = currentBoardLayout[id] + color;
         setCurrentBoardLayout(newBoardLayout);
-        checkIsGameFinished(newBoardLayout) 
         setIsRedTurn(!isRedTurn);
+        checkIsGameFinished(newBoardLayout) 
     }
 
     function checkIsGameFinished(boardLayout:string[]){
-        console.log('checking if game is finished')
-        //check if columns has a winner:
+        //check if board is full and there is no winner;
 
+        //check if columns has a winner:
         boardLayout.forEach(column=>{
             if (column.includes('rrrr')) {
                 setWinnerColor('r');
@@ -42,6 +41,46 @@ export default function Game(){
                 return true
             }
         });
+
+        //check if rows has a winner
+        for (let y:number = 0; y<NUMBER_OF_ROWS; y++) {
+            let rowStr = '';
+            for (let x:number = 0; x<NUMBER_OF_COLUMNS; x++) {
+                rowStr += boardLayout[x][y];
+            }
+            if (rowStr.includes('yyyy')) {
+                setWinnerColor('y');
+                setIsGameFinished(true);
+                return true
+            }
+            if (rowStr.includes('rrrr')) {
+                setWinnerColor('r');
+                setIsGameFinished(true);
+                return true
+            }
+        } 
+        //check if there is a bottom left to top right diagonal winner
+
+        for (let j:number = -4; j<4; j++) {
+            let diagStr='';
+            for (let i:number = 0; i<NUMBER_OF_ROWS; i++) {
+                if (boardLayout[i] && boardLayout[i][i+j]){
+                    diagStr += boardLayout[i][i+j];
+                } else 
+                    diagStr += ' ';      
+            }
+            if (diagStr.includes('yyyy')) {
+                setWinnerColor('y');
+                setIsGameFinished(true);
+                return true
+            }
+            if (diagStr.includes('rrrr')) {
+                setWinnerColor('r');
+                setIsGameFinished(true);
+                return true
+            }
+        }
+        
         return false
         
     }
@@ -69,7 +108,7 @@ export default function Game(){
 function BoardColumn({columnStr}:{ columnStr:string }){
     const NUMBER_OF_ROWS = 7;
     let tiles:any = [];
-    for (let tileIdx = 0; tileIdx<NUMBER_OF_ROWS; tileIdx++) {
+    for (let tileIdx:number = 0; tileIdx<NUMBER_OF_ROWS; tileIdx++) {
         if (columnStr[tileIdx]==='y')
             tiles.push(
                 <div style={{ height: '30px', width: '30px', border: '1px black solid',  borderRadius:'15px', backgroundColor: 'yellow'  }} key={tileIdx}></div>
